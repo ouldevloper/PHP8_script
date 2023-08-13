e922a84878b84e65f82fb86545043f295b71437a
user <user@localhost.localdomain>
2023-08-13 10:58:22 +0300

    --

--- PHP_rabbitMQ/receiver.php
+++ PHP_rabbitMQ/receiver.php
@@ -1,19 +1,24 @@
 <?php
 
-
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
-use PhpAmqpLib\Message\AMQPMessage;
 
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
 $channel->queue_declare('hello', false, false, false, false);
 
-$msg = new AMQPMessage('Hello World!');
-$channel->basic_publish($msg, '', 'hello');
+echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
-echo " [x] Sent 'Hello World!'\n";
+$callback = function ($msg) {
+    echo ' [x] Received ', $msg->body, "\n";
+};
+
+$channel->basic_consume('hello', '', false, true, false, false, $callback);
+
+while ($channel->is_open()) {
+    $channel->wait();
+}
 
 $channel->close();
 $connection->close();
---
+++ PHP_rabbitMQ/vendor/composer/installed.php
@@ -0,0 +1,67 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5696ff55e15d0be12b2c5779d0561ea959cf9278',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5696ff55e15d0be12b2c5779d0561ea959cf9278',
+    ),
+    'paragonie/constant_time_encoding' => 
+    array (
+      'pretty_version' => 'v2.6.3',
+      'version' => '2.6.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '58c3f47f650c94ec05a151692652a868995d2938',
+    ),
+    'paragonie/random_compat' => 
+    array (
+      'pretty_version' => 'v9.99.100',
+      'version' => '9.99.100.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '996434e5492cb4c3edcb9168db6fbb1359ef965a',
+    ),
+    'php-amqplib/php-amqplib' => 
+    array (
+      'pretty_version' => 'v3.5.4',
+      'version' => '3.5.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1aecbd182b35eb039667c50d7d92d71f105be779',
+    ),
+    'phpseclib/phpseclib' => 
+    array (
+      'pretty_version' => '3.0.21',
+      'version' => '3.0.21.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4580645d3fc05c189024eb3b834c6c1e4f0f30a1',
+    ),
+    'videlalvaro/php-amqplib' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v3.5.4',
+      ),
+    ),
+  ),
+);
